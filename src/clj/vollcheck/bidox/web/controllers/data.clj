96ebdae5e7cdde25de7d2886b3cf94c4d2f59b73
vollcheck <jaw@juxt.pro>
2023-06-08 18:19:10 +0200

    put the test data in the repository - useful when playing around with xtdb

---
+++ src/clj/vollcheck/bidox/web/controllers/data.clj
@@ -0,0 +1,12 @@
+(ns vollcheck.bidox.web.controllers.data)
+
+(def documents-data
+  [{:xt/id 1000
+    :doc/title "marketing mix"
+    :doc/content "something something marketing mix"}
+   {:xt/id 1001
+    :doc/title "yeah another doc"
+    :doc/content "blahblahblah"}
+   {:xt/id 1002
+    :doc/title "zero page"
+    :doc/content "reach out to https://en.wikipedia.org/wiki/Zero_page"}])
